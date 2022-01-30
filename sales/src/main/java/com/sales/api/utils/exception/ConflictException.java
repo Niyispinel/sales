a3e4f1464ceda96cf6796b022174fde0ccfd6e05
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2022-01-30 20:15:28 +0100

    gbeminiyi

---
+++ sales/src/main/java/com/sales/api/utils/exception/ConflictException.java
@@ -0,0 +1,9 @@
+package com.sales.api.utils.exception;
+
+public class ConflictException extends AbstractException {
+
+    public ConflictException(String code, String message) {
+        super(code, message);
+
+    }
+}
